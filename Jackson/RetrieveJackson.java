8296740164bc00575d966dbc202d2544d63f4f3a
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2017-07-07 14:53:17 +0200

    riproviamo

--- Jackson/RetrieveJackson.java
+++ Jackson/RetrieveJackson.java
@@ -2,11 +2,6 @@
  * Created by unibo on 07/07/2017.
  */
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
 public class RetrieveJackson{
     public static void main(String[] args) {
         System.out.println("asdfads");
